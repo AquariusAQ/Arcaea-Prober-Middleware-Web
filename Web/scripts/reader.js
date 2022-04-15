5b292ac9c6c9c158303169038b21a55f33945c04
10195501404 <10195501404@stu.ecnu.edu.cn>
2022-04-15 11:48:13 +0800

    fix: duplicate song data

--- Web/scripts/reader.js
+++ Web/scripts/reader.js
@@ -41,9 +41,15 @@ function get_info(qq, callback) {
                     result['username'] = rich_info['userinfo']['name'];
                     result['b30'] = [];
                     result['update_time'] = update_time;
-                    let song_count = Math.min(30, rich_info['scores'].length)
-                    for (let index = 0; index < song_count; index++) {
+                    // let song_count = Math.min(30, rich_info['scores'].length)
+                    var cur_id = "";
+                    var song_index = 0;
+                    for (let index = 0; index < rich_info['scores'].length; index++) {
+                        if (song_index == 30) break;
                         const element = rich_info['scores'][index];
+                        if (element['song_id'] == cur_id) continue;
+                        cur_id = element['song_id'];
+                        song_index += 1;
                         result['b30'].push({
                             song_id: element['song_id'],
                             song: rich_info['song_title'][element['song_id']]['en'],
@@ -58,6 +64,22 @@ function get_info(qq, callback) {
                             constant: element['constant'],
                         });
                     }
+                    // for (let index = 0; index < song_count; index++) {
+                    //     const element = rich_info['scores'][index];
+                    //     result['b30'].push({
+                    //         song_id: element['song_id'],
+                    //         song: rich_info['song_title'][element['song_id']]['en'],
+                    //         difficulty: diff_list[element['difficulty']],
+                    //         score: element['score'],
+                    //         shiny_perfect_count: element['shiny_perfect_count'],
+                    //         perfect_count: element['perfect_count'],
+                    //         near_count: element['near_count'],
+                    //         miss_count: element['miss_count'],
+                    //         clear_type: clear_list[element['clear_type']],
+                    //         rating: element['rating'],
+                    //         constant: element['constant'],
+                    //     });
+                    // }
                     callback({code:200, msg:'获取成功', data:result});
                 }
             });
